646e9cde51c5b96fa748ee015e6e2c8de061d614
yuehlin <yuehlin@shapeways.com>
2018-10-18 04:15:28 -0700

    House price

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,30 @@
 <template>
   <div id="app">
-    <!-- <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul> -->
     <b-navbar toggleable="md" type="dark" variant="info" :sticky="true">
-
       <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-
       <b-navbar-brand href="#">NavBar</b-navbar-brand>
-
       <b-collapse is-nav id="nav_collapse">
-
         <b-navbar-nav>
           <b-nav-item href="#line-chart">Line Chart</b-nav-item>
-          <b-nav-item href="#" disabled>Disabled</b-nav-item>
         </b-navbar-nav>
-
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
-
-          <b-nav-form>
-            <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search"/>
-            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-          </b-nav-form>
-
-          <b-nav-item-dropdown text="Lang" right>
-            <b-dropdown-item href="#">EN</b-dropdown-item>
-            <b-dropdown-item href="#">ES</b-dropdown-item>
-            <b-dropdown-item href="#">RU</b-dropdown-item>
-            <b-dropdown-item href="#">FA</b-dropdown-item>
-          </b-nav-item-dropdown>
-
-          <b-nav-item-dropdown right>
-            <!-- Using button-content slot -->
-            <template slot="button-content">
-              <em>User</em>
-            </template>
-            <b-dropdown-item href="#">Profile</b-dropdown-item>
-            <b-dropdown-item href="#">Signout</b-dropdown-item>
-          </b-nav-item-dropdown>
-        </b-navbar-nav>
-
       </b-collapse>
     </b-navbar>
     
-    <LineChart id="line-chart"  />
+    <b-container id="house-price-section">
+      <b-row>
+        <HousePrice />
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-import LineChart from './components/LineChart.vue';
+import HousePrice from './components/HousePrice.vue';
 
 export default {
   name: 'app',
   components: {
-    LineChart,
+    HousePrice,
   },
 }
 </script>
@@ -81,7 +38,7 @@ export default {
   color: #2c3e50;
 }
 
-#line-chart {
+#house-price-section {
   margin-top: 32px;
 }
 
